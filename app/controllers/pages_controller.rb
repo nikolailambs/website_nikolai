a287da5d5fc57f82224278ea0eb56a61a806eeca
nikolai lambsdorff <nikolai.lambsdorff@gmx.de>
2018-07-23 17:33:51 +0200

    changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,7 +11,7 @@ class PagesController < ApplicationController
     @examples.push(Playful.first)
     @examples.push(Coding.first)
 
-    @description = ["This is an example of how a modern page could look like. It's a very simple and clean page, that could perfectly show features of apps or products as well as consultants or laywers.", "The art example is a very nice page for photographers, architects or artists. This page is based on one or more photos as the background with fewer text.", "Playful is a more unusual way of how you could implement a more interesting and exotic navigation menu. If you are a game developer or if you just want to gain more attention with a playful website.", "The last example is a very funny and also exotic website, that get's developed and coded during while you are on the page. But see for yourself..."]
+    @description = ["This is an example of how a modern page could look like. It's a very simple and clean page, that could perfectly show features of apps or products as well as for consultants or laywers.", "The art example is a very nice page for photographers, architects or artists. This page is based on one or more photos as the background with fewer text.", "Playful is a more unusual way of how you could implement a more interesting and exotic navigation menu. If you are a game developer or if you just want to gain more attention with a playful website.", "The last example is a very funny and also exotic website, that get's developed and coded during while you are on the page. But see for yourself..."]
 
     @rand_top = [10, 40, 40, 40, 60, 70, 70, 10, 60]
     @rand_left = [60, 80, 20, 60, 80, 40, 70, 8, 10]
