d92781cb856f58332275ac2265df76b554dc8314
nikolai lambsdorff <nikolai.lambsdorff@gmx.de>
2019-02-21 11:58:24 +0100

    sd

---
+++ app/controllers/privacy_controller.rb
@@ -0,0 +1,10 @@
+class PrivacyController < ApplicationController
+  before_action :set_example, only: [:show, :edit, :update, :destroy]
+  skip_before_action :authenticate_user!, only: [:show, :index]
+
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :arts
   resources :classics
   resources :examples
+  resources :privacy
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
